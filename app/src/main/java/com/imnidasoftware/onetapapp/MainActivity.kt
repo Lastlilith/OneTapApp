3d5cbcf968cddda55fd25a6f06f6b36e9dbeb3d1
Lastlilith <lastlilith.2014@gmail.com>
2023-01-21 21:32:48 +0900

    Hilt setup

--- app/src/main/java/com/imnidasoftware/onetapapp/MainActivity.kt
+++ app/src/main/java/com/imnidasoftware/onetapapp/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.imnidasoftware.onetapapp.navigation.SetupNavGraph
 import com.imnidasoftware.onetapapp.ui.theme.OneTapAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/imnidasoftware/onetapapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.imnidasoftware.onetapapp.util
+
+object Constants {
+
+    const val BASE_URL = "http://10.0.2.2:8080"
+}
