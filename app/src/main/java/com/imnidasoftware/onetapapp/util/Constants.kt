f479210041c4f3e52b35d575f81ba0ec11cc1708
Lastlilith <lastlilith.2014@gmail.com>
2023-01-24 15:37:19 +0900

    Created signUp and signIn funcs

--- app/src/main/java/com/imnidasoftware/onetapapp/util/Constants.kt
+++ app/src/main/java/com/imnidasoftware/onetapapp/util/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val BASE_URL = "http://10.0.2.2:8080"
     const val PREFERENCES_SIGNED_IN_KEY = "signed_in_key"
     const val PREFERENCES_NAME = "app_preferences"
+    const val CLIENT_ID = "132893197858-9btt6lcu4gol9aogdl5nfev44m2249ic.apps.googleusercontent.com"
 }
